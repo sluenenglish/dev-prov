30c4b4233860e1f7c93dec2cd95333d9ea6da5d8
Sam Luen-English <sluenenglish@gmail.com>
2018-01-01 16:15:15 +0000

    Fix users

--- provision.sh
+++ provision.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get upgrade
 sudo apt-get install -y python-pip
 
 sudo pip install ansible
 
-sudo ansible-galaxy install \
+ansible-galaxy install \
     cmprescott.chrome \
     angstwad.docker_ubuntu \
     tersmitten.pycharm
